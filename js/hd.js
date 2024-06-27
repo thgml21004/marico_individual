437c91087a6e6b5bf8d03439f585de65cf73eff1
thgml21004 <thgml21004@gmail.com>
2024-06-27 17:00:11 +0900

    팀프로젝트 마리코 개인 코딩 정리

--- js/hd.js
+++ js/hd.js
@@ -1,7 +1,7 @@
 const hd = {
     hdLogoS : {
         text : "myrealtrip",
-        imgUrl : "/img/hdLogo.svg",
+        imgUrl : "img/hdLogo.svg",
         href : "/",
     },
     hdGnbS : [
@@ -85,45 +85,44 @@ const hd = {
                 }
             ]
         },
-    ],
-    hdIconMenuS : [
-        {
-            icon : "/img/hdSearch.svg",
-            text : "검색",
-            id : "hdSearchBtnS",
-            hdSearchS : {
-                title : "실시간 인기",
-                text : [
-                    "관광지",
-                    "카페",
-                    "패키지 여행 예약",
-                    "추천 코스",
-                    "맛집",
-                    "황남빵",
-                    "경주월드",
-                    "호텔",
-                    "한복대여",
-                    "야경"
-                ]
-            }
-        },
-        {
-            icon : "/img/hdLogin.svg",
-            text : "마이페이지",
-            id : "",
-            hdSearchS  : {
-                title : "",
-                text : ""
-            }
-        }
     ]
+    // hdIconMenuS : [
+    //     {
+    //         icon : "img/hdSearch.svg",
+    //         text : "검색",
+    //         id : "hdSearchBtnS",
+    //         hdSearchS : {
+    //             title : "실시간 인기",
+    //             text : [
+    //                 "관광지",
+    //                 "카페",
+    //                 "패키지 여행 예약",
+    //                 "추천 코스",
+    //                 "맛집",
+    //                 "황남빵",
+    //                 "경주월드",
+    //                 "호텔",
+    //                 "한복대여",
+    //                 "야경"
+    //             ]
+    //         }
+    //     },
+    //     {
+    //         icon : "img/hdLogin.svg",
+    //         text : "마이페이지",
+    //         id : "",
+    //         hdSearchS  : {
+    //             title : "",
+    //             text : ""
+    //         }
+    //     }
+    // ]
 }
 
 window.onload = function() {
     let hdTag = '';
 
-    hdTag += `<h1 class="mb-0 ${Object.keys(hd)[0]}"><a href="${hd.hdLogoS.href}"><img src="${hd.hdLogoS.imgUrl}" alt="${hd.hdLogoS.text}"></a></h1>
-        <ul class="d-flex ps-0 mb-0 ${Object.keys(hd)[1]}">`
+    hdTag += `<ul class="d-flex ps-0 mb-0 ${Object.keys(hd)[1]}">`
     for(x in hd.hdGnbS) {
         hdTag += `<li class="position-relative">
             <a href="${hd.hdGnbS[x].href}">${hd.hdGnbS[x].text}</a>
@@ -137,27 +136,27 @@ window.onload = function() {
             </li>`
     }
     hdTag += `</ul>`;
-    hdTag += `<ul class="d-flex justify-content-end ps-0 mb-0 ${Object.keys(hd)[2]}">`
-    for(e in hd.hdIconMenuS) {
-        hdTag += `<li>
-            <i><img src="${hd.hdIconMenuS[e].icon}"></i>
-            <span id="${hd.hdIconMenuS[e].id}" class="visually-hidden">${hd.hdIconMenuS[e].text}</span>`;
-            hdTag += `<div class="${Object.keys(hd.hdIconMenuS[e])[3]}">
-                <p>${hd.hdIconMenuS[e].hdSearchS.title}</p>
-                    <div class="d-flex">
-                        <ul class="ps-0 mb-0">`
-                            for(r in hd.hdIconMenuS[e].hdSearchS.text) {
-                                hdTag += `<li>
-                                    <em>${ parseInt(r) + 1}</em>
-                                    ${hd.hdIconMenuS[e].hdSearchS.text[r]}
-                                </li>`
-                            }
-                hdTag += `</ul>
-                    </div>
-                </div>
-            </li>`;
-    }
-    hdTag += `</ul>`;
+    // hdTag += `<ul class="d-flex justify-content-end ps-0 mb-0 ${Object.keys(hd)[2]}">`
+    // for(e in hd.hdIconMenuS) {
+    //     hdTag += `<li>
+    //         <i><img src="${hd.hdIconMenuS[e].icon}"></i>
+    //         <span id="${hd.hdIconMenuS[e].id}" class="visually-hidden">${hd.hdIconMenuS[e].text}</span>`;
+    //         hdTag += `<div class="${Object.keys(hd.hdIconMenuS[e])[3]}">
+    //             <p>${hd.hdIconMenuS[e].hdSearchS.title}</p>
+    //                 <div class="d-flex">
+    //                     <ul class="ps-0 mb-0">`
+    //                         for(r in hd.hdIconMenuS[e].hdSearchS.text) {
+    //                             hdTag += `<li>
+    //                                 <em>${ parseInt(r) + 1}</em>
+    //                                 ${hd.hdIconMenuS[e].hdSearchS.text[r]}
+    //                             </li>`
+    //                         }
+    //             hdTag += `</ul>
+    //                 </div>
+    //             </div>
+    //         </li>`;
+    // }
+    // hdTag += `</ul>`;
     
-    document.querySelector("#hd > div").innerHTML = hdTag;
+    document.querySelector(".hdGnbS").innerHTML = hdTag;
 };
